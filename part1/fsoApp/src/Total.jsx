0f59f34c23d8323fe017e2de73a97148cee1219b
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-06-15 11:43:51 +0200

    1.4

--- part1/fsoApp/src/Total.jsx
+++ part1/fsoApp/src/Total.jsx
@@ -1,8 +1,15 @@
 const Total = (props) => {
 
+  const parts = props.parts
+  let total = 0
+  parts.forEach(element => {
+    let exercise = element.exercises
+    total += exercise
+  });
+
   return (
     <div>
-       <p>Number of exercises {props.total}</p>
+       <p>Number of exercises {total}</p>
     </div>
   )
 }
