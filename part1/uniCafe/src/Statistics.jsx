88d6f1657d499245b947523f615a824e99d4a08c
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-06-19 19:36:26 +0200

    1.11

--- part1/uniCafe/src/Statistics.jsx
+++ part1/uniCafe/src/Statistics.jsx
@@ -5,12 +5,16 @@ const Statistics = ({good,neutral,bad,total,average,positivePercentage}) => {
 
   return (
     <>
+      <table>
+      <tbody>
       <StatisticLine text="good"  value ={good} />
       <StatisticLine text="neutral" value ={neutral} />
       <StatisticLine text="bad" value ={bad} />
       <StatisticLine text="total" value ={total} />
       <StatisticLine text="average" value ={average} />
       <StatisticLine text="positive"  value ={positivePercentage +" " + "%"} />
+      </tbody>
+      </table>
     </>
   )
 }
