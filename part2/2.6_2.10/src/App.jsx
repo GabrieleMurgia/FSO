2fd7656dcf2336d665290e06d69c24722596ebb4
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-06-21 18:19:45 +0200

    2.6

---
+++ part2/2.6_2.10/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  function handleSumbmitNewName(e){
+    e.preventDefault()
+    setPersons(persons.concat({
+      name:newName
+    }))
+  }
+
+  function handleChangeNewName(e){
+    setNewName(e.target.value)
+
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChangeNewName}/>
+        </div>
+        <div>
+          <button onClick={handleSumbmitNewName} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>{
+        return <p key={person.name + Math.random()}>{person.name}</p>
+      })}
+    </div>
+  )
+}
+
+export default App
